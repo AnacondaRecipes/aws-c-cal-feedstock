74974359b1628e419eac9e705bd1c09021daa238
PatrykKups <pkups@anaconda.com>
2025-06-26 16:49:29 +0200

    aws-c-cal 0.9.2  (#3)
    * PKG-8546: aws-c-cal 0.9.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,14 +4,25 @@ set -ex
 
 mkdir build
 pushd build
+
+EXTRA_CMAKE_ARGS=""
+
+if [[ ${target_platform} == linux-* ]]; then
+  EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DUSE_OPENSSL=ON"
+fi
+
 cmake ${CMAKE_ARGS} -GNinja \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_INSTALL_LIBDIR=lib \
-  -DENABLE_TESTING=OFF \
+  -DENABLE_TESTING=ON \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=ON \
-  -DUSE_OPENSSL=ON \
+  -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+  ${EXTRA_CMAKE_ARGS} \
   ..
-ninja install
+
+cmake --build . --config Release --target install
+
+ctest --output-on-failure -j${CPU_COUNT}
 popd
