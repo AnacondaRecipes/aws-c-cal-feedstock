b2549277634b61488759eea75988ca448d5ae0e9
PatrykKups <pkups@anaconda.com>
2025-06-26 08:28:57 +0200

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,13 @@ set -ex
 
 mkdir build
 pushd build
+
+EXTRA_CMAKE_ARGS=""
+
+if [[ ${target_platform} == linux-* ]]; then
+  EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DUSE_OPENSSL=ON"
+fi
+
 cmake ${CMAKE_ARGS} -GNinja \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
@@ -12,9 +19,7 @@ cmake ${CMAKE_ARGS} -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=ON \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-if [[ ${target_platform} == linux-* ]]; then
-  -DUSE_OPENSSL=ON \
-fi
+  ${EXTRA_CMAKE_ARGS} \
   ..
 
 cmake --build . --config Release --target install
